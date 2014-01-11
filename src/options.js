4995a75a75f634246496b8b47fd3422c443aa35b
Marcus McCurdy <mmccurdy@50onred.com>
2014-01-11 08:50:29 -0500

    Minor update to options

--- src/options.js
+++ src/options.js
@@ -1,25 +1,24 @@
-$(function() {
+$(function () {
     var $price_rise = $('#price-rise');
     var $price_drop = $('#price-drop');
 
     function save_options() {
-        var alerts = {};
-        alerts.price_rise = $price_rise.val();
-        alerts.price_drop = $price_drop.val();
+        var alerts = {
+            price_rise: $price_rise.val(),
+            price_drop: $price_drop.val()
+        };
 
-      chrome.storage.sync.set(alerts, function() {
-        // Update status to let user know options were saved.
-        var status = document.getElementById("status");
-        status.innerHTML = "Options Saved.";
-        setTimeout(function() {
-            status.innerHTML = "";
-        }, 750);   
-      });
+        chrome.storage.sync.set(alerts, function () {
+            var status = document.getElementById("status");
+            status.innerHTML = "Options Saved.";
+            setTimeout(function () {
+                status.innerHTML = "";
+            }, 750);
+        });
     }
-    
-    // Restores select box state to saved value from localStorage.
+
     function restore_options() {
-        chrome.storage.sync.get(['price_rise', 'price_drop'], function(items) {
+        chrome.storage.sync.get(['price_rise', 'price_drop'], function (items) {
             if (items.price_rise) {
                 $price_rise.val(items.price_rise);
             }
@@ -30,7 +29,7 @@ $(function() {
     }
 
     restore_options();
-    $('#options-form').submit(function(e) {
+    $('#options-form').submit(function (e) {
         e.preventDefault();
         save_options();
     });
